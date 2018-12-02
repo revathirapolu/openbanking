c72952b2b798a93e82e9c8cf9bdaa077ac272994
ramrapolu <java8dev@outlook.com>
2018-12-02 15:44:55 +0000

    Initial commit

---
+++ src/main/java/com/ram/test/openbanking/OpenbankingApplication.java
@@ -0,0 +1,12 @@
+package com.ram.test.openbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenbankingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ram/test/openbanking/OpenbankingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ram.test.openbanking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpenbankingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
